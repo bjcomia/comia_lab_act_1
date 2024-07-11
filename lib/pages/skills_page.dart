6c61b16b9bf5b5f2cfdbf4b81e27929fae397eeb
Benedict John Comia <bjcomia00@gmail.com>
2024-07-11 20:25:08 +0800

    Added AvatarGlow package and implemented skills page with hard and soft skills buttons

--- lib/pages/skills_page.dart
+++ lib/pages/skills_page.dart
@@ -1,4 +1,6 @@
+import 'package:comia_lab_act_1/tempData/skilss_data.dart';
 import 'package:flutter/material.dart';
+import 'package:avatar_glow/avatar_glow.dart';
 
 class SkillsPage extends StatefulWidget {
   const SkillsPage({super.key});
@@ -10,6 +12,195 @@ class SkillsPage extends StatefulWidget {
 class _SkillsPageState extends State<SkillsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        //Hard Skills
+        AvatarGlow(
+          startDelay: const Duration(milliseconds: 1000),
+          glowColor: Theme.of(context).colorScheme.secondary,
+          glowShape: BoxShape.circle,
+          glowRadiusFactor: 0.15,
+          curve: Curves.fastOutSlowIn,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                BoxShadow(
+                  color: Theme.of(context).colorScheme.shadow.withOpacity(0.5),
+                  offset: const Offset(1, 10),
+                  blurRadius: 13,
+                  spreadRadius: 2,
+                )
+              ],
+            ),
+            child: ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text(
+                        "Technical Skills",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontSize: 30,
+                        ),
+                      ),
+                      backgroundColor:
+                          Theme.of(context).colorScheme.onPrimaryContainer,
+                      actionsAlignment: MainAxisAlignment.center,
+                      content: SizedBox(
+                        height: 130,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            for (int index = 0;
+                                index < hardSkills.length;
+                                index++)
+                              Text(
+                                "\u2022 ${hardSkills[index]}",
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                  fontSize: 17,
+                                ),
+                                textAlign: TextAlign.left,
+                              ),
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, 'Close'),
+                          child: Text(
+                            'Close',
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(
+                    Theme.of(context).colorScheme.primary),
+                fixedSize: const WidgetStatePropertyAll(
+                  Size(200, 200),
+                ),
+              ),
+              child: Text(
+                "Technical Skills",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 70),
+
+        //Soft Skills
+        AvatarGlow(
+          startDelay: const Duration(milliseconds: 1000),
+          glowColor: Theme.of(context).colorScheme.secondary,
+          glowShape: BoxShape.circle,
+          glowRadiusFactor: 0.15,
+          curve: Curves.fastOutSlowIn,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                BoxShadow(
+                  color: Theme.of(context).colorScheme.shadow.withOpacity(0.5),
+                  offset: const Offset(1, 10),
+                  blurRadius: 13,
+                  spreadRadius: 2,
+                )
+              ],
+            ),
+            child: ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text(
+                        "Non-Technical Skills",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontSize: 30,
+                        ),
+                      ),
+                      backgroundColor:
+                          Theme.of(context).colorScheme.onPrimaryContainer,
+                      actionsAlignment: MainAxisAlignment.center,
+                      content: SizedBox(
+                        height: 130,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            for (int index = 0;
+                                index < softSkills.length;
+                                index++)
+                              Text(
+                                "\u2022 ${softSkills[index]}",
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                  fontSize: 17,
+                                ),
+                                textAlign: TextAlign.left,
+                              ),
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, 'Close'),
+                          child: Text(
+                            'Close',
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(
+                    Theme.of(context).colorScheme.primary),
+                fixedSize: const WidgetStatePropertyAll(
+                  Size(200, 200),
+                ),
+              ),
+              child: Text(
+                "Non-Technical Skills",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
---
+++ lib/tempData/skilss_data.dart
@@ -0,0 +1,15 @@
+const hardSkills = [
+  "Programming",
+  "Analytical Skills",
+  "Database Management",
+  "Photo Editing",
+  "Video Editing",
+];
+
+const softSkills = [
+  "Time Management",
+  "Organizational Skills",
+  "Critical Thinking",
+  "Problem-Solving",
+  "Collaboration",
+];
