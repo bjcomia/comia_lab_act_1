2105f27bba28c042ed6e1e700d4cb82fbc0ec37c
Benedict John Comia <bjcomia00@gmail.com>
2024-07-12 00:05:31 +0800

    Updated ContactDetailsPage and WelcomePage UI, added Ionicons dependency

--- lib/pages/contact_details_page.dart
+++ lib/pages/contact_details_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 class ContactDetailsPage extends StatefulWidget {
   const ContactDetailsPage({super.key});
@@ -10,6 +11,73 @@ class ContactDetailsPage extends StatefulWidget {
 class _ContactDetailsPageState extends State<ContactDetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ContactTemplate(icon: Icons.email, detail: "bjcomia00@gmail.com"),
+            ContactTemplate(
+                icon: Icons.phone, detail: "09917745442 / 09565224774"),
+            ContactTemplate(
+                icon: Ionicons.logo_facebook,
+                detail: "https://www.facebook.com/bjcomia016"),
+            ContactTemplate(
+                icon: Ionicons.logo_twitter,
+                detail: "https://x.com/bene_comia"),
+            ContactTemplate(
+                icon: Ionicons.logo_instagram,
+                detail: "https://www.instagram.com/b.comia/"),
+            ContactTemplate(
+                icon: Ionicons.logo_github,
+                detail: "https://github.com/bjcomia")
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ContactTemplate extends StatefulWidget {
+  final IconData icon;
+  final String detail;
+  const ContactTemplate({super.key, required this.icon, required this.detail});
+
+  @override
+  State<ContactTemplate> createState() => _ContactTemplateState();
+}
+
+class _ContactTemplateState extends State<ContactTemplate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+      padding: const EdgeInsets.all(10),
+      height: 50,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.onPrimaryContainer,
+        borderRadius: BorderRadius.circular(14),
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).colorScheme.shadow,
+            spreadRadius: 1,
+            blurRadius: 1,
+            offset: const Offset(0, 1),
+          )
+        ],
+      ),
+      child: Row(
+        children: [
+          Icon(widget.icon),
+          const SizedBox(width: 15),
+          Text(
+            widget.detail,
+            style: TextStyle(
+                color: Theme.of(context).colorScheme.secondary, fontSize: 14),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -12,24 +12,44 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text("Welcome to My Laboratory Activity 1"),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return const HomePage();
-                      },
-                    ),
-                  );
-                },
-                child: const Text("Proceed"))
-          ],
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome to My Laboratory Activity 1",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return const HomePage();
+                        },
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      shadowColor: Theme.of(context).colorScheme.shadow,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      backgroundColor: Theme.of(context).colorScheme.primary),
+                  child: Text(
+                    "Proceed",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.secondary),
+                  ))
+            ],
+          ),
         ),
       ),
     );
